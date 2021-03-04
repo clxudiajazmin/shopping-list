baf573b94eea24e0f4a0f72d83b8859d4ca9e611
Claudia Jazmín Soria Saavedra <clxudiajazmin@gmail.com>
2021-03-04 09:33:38 +0100

    Checked Items

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { EventEmitter } from "@angular/core";
+import { Item } from 'src/app/models/item';
 
 @Component({
   selector: 'app-item',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemComponent implements OnInit {
 
+  @Input() item: Item = new Item();
+  @Output() deleteItem: EventEmitter<Item> = new EventEmitter();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onDelete(item: Item){
+    this.deleteItem.emit(item);
+  }
+
+  onToggle(item: Item){
+    item.completed = !item.completed;
   }
 
 }
