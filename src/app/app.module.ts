7dc9dab442031477b8f9b04b77af9cd9e55a32f8
Claudia Jazmín Soria Saavedra <clxudiajazmin@gmail.com>
2021-03-04 15:16:16 +0100

    Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ItemsComponent } from './components/items/items.component';
 import { ItemComponent } from './components/item/item.component';
+import { AdditemComponent } from './components/additem/additem.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ItemsComponent,
-    ItemComponent
+    ItemComponent,
+    AdditemComponent
   ],
   imports: [
     BrowserModule,
